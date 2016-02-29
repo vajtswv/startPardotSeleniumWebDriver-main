221e40153a4788119a06af48848c73d24e389d96
vajtswv <ks6908@msn.com>
2016-02-29 15:08:05 -0500

    remove comment

--- src/test/java/com/kevintest/webdriver/MyTest.java
+++ src/test/java/com/kevintest/webdriver/MyTest.java
@@ -19,14 +19,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  *
  * Created by ksong on 2/27/2016.
  * /////////////////////////////////Test Steps//////////////////////////////////
- * 1. Create a list with a random name (Marketing > Segmentation > Lists)
- 2. Attempt to create another list with that same name and ensure the system correctly gives a validation failure
- 3. Rename the original list
- 4. Ensure the system allows the creation of another list with the original name now that the original list is renamed
- 5. Create a new prospect (Prospect > Prospect List)
- 6. Add your new prospect to the newly created list
- 7. Ensure the new prospect is successfully added to the list upon save
- 8. Send a text only email
+ *
  */
 public class MyTest {
 
