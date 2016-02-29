b5fdc6b27558891eb04d2693adbce13fe25cf220
vajtswv <ks6908@msn.com>
2016-02-29 15:56:22 -0500

    remove comment

--- src/test/java/com/kevintest/webdriver/MyTest.java
+++ src/test/java/com/kevintest/webdriver/MyTest.java
@@ -7,6 +7,8 @@ import java.lang.String;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -23,33 +25,33 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class MyTest {
 
+    static WebDriver _driver;
+
    @Test
-    public void startWebDriver(){
+    public void beginDriver(){
 
-       //WebDriver driver = new FirefoxDriver();
-       String strName = GenerateNames();
-       String str = SegmentList(strName);//first list
-       String str2 = SegmentList(strName);//second list
+        _driver = new FirefoxDriver();
+        String strName = GenerateNames();
+        String str = SegmentList(strName);//first list
+        String str2 = SegmentList(strName);//second list
 
+       closeFirefoxBrowser();
     }
 
     //generate names
     public static String SegmentList(String str){
 
-        //create a new instance of firefox driver
-        //create a new instance of firefox driver
-       WebDriver driver = new FirefoxDriver();
         //wait for page to load
         // Put a Implicit wait, this means that any search for elements on the page
         //could take the time the implicit wait is set for before throwing exception
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://pi.pardot.com/");
-        driver.manage().window().maximize();
+        _driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        _driver.get("https://pi.pardot.com/");
+        _driver.manage().window().maximize();
 
         //declare elements
-        WebElement username = driver.findElement(By.id("email_address"));
-        WebElement password = driver.findElement(By.id("password"));
-        WebElement login = driver.findElement(By.cssSelector("input[type = 'submit']"));
+        WebElement username = _driver.findElement(By.id("email_address"));
+        WebElement password = _driver.findElement(By.id("password"));
+        WebElement login = _driver.findElement(By.cssSelector("input[type = 'submit']"));
 
         //perform login actions
         username.sendKeys("pardot.applicant@pardot.com");
@@ -57,32 +59,32 @@ public class MyTest {
         login.click();
 
         //GOTO Marketing>Segmentation>Lists
-        driver.findElement(By.linkText("Marketing")).click();
-        driver.findElement(By.linkText("Segmentation")).click();
-        driver.findElement(By.linkText("Lists")).click();
+        _driver.findElement(By.linkText("Marketing")).click();
+        _driver.findElement(By.linkText("Segmentation")).click();
+        _driver.findElement(By.linkText("Lists")).click();
 
         //create random lists
-        driver.findElement(By.id("listxistx_link_create")).click();
+        _driver.findElement(By.id("listxistx_link_create")).click();
 
         //enter name
-        WebElement name = driver.findElement(By.id("name"));
+        WebElement name = _driver.findElement(By.id("name"));
         name.sendKeys(str);
 
         //enable Dynamic List
-        driver.findElement(By.id("enable_dynamic_list")).click();
+        _driver.findElement(By.id("enable_dynamic_list")).click();
         //set rules
-        driver.findElement(By.id("save_information")).click();
+        _driver.findElement(By.id("save_information")).click();
 
         //check for duplicate name
-        WebElement thiserror = driver.findElement(By.id("error_for_name"));
-        if(driver.findElement(By.id("error_for_name")).isDisplayed()){
+        WebElement thiserror = _driver.findElement(By.id("error_for_name"));
+        if(_driver.findElement(By.id("error_for_name")).isDisplayed()){
             System.out.println(thiserror.getText());
-            WebElement name2 = driver.findElement(By.id("name"));
+            WebElement name2 = _driver.findElement(By.id("name"));
             String tempName2 = GenerateNames();
             name2.sendKeys("");
             name2.sendKeys(tempName2);
             //set rules
-            driver.findElement(By.id("save_information")).click();
+            _driver.findElement(By.id("save_information")).click();
             str = tempName2;
             System.out.println("Name is unique:" +str);
         }else{
@@ -90,65 +92,62 @@ public class MyTest {
         }
 
         //Create prospects
-        driver.findElement(By.linkText("Prospects")).click();
-        driver.findElement(By.linkText("Prospect List")).click();
-        driver.findElement(By.id("pr_link_create")).click();//add prospect
+        _driver.findElement(By.linkText("Prospects")).click();
+        _driver.findElement(By.linkText("Prospect List")).click();
+        _driver.findElement(By.id("pr_link_create")).click();//add prospect
 
         //prospect/////////////////////////////////////////////////////
-        WebElement email = driver.findElement(By.id("email"));
+        WebElement email = _driver.findElement(By.id("email"));
         email.sendKeys(str+"@pardot.com");
-        WebElement campain = driver.findElement(By.id("campaign_id"));
+        WebElement campain = _driver.findElement(By.id("campaign_id"));
         campain.click();
         campain.sendKeys("Adil Yellow Jackets");
-        WebElement profile = driver.findElement(By.id("profile_id"));
+        WebElement profile = _driver.findElement(By.id("profile_id"));
         profile.click();
         profile.sendKeys("Adil Yellow Jackets 1");
-        WebElement score = driver.findElement(By.id("score"));
+        WebElement score = _driver.findElement(By.id("score"));
         score.sendKeys("1");
-        WebElement Lists = driver.findElement(By.id("toggle-inputs-lists-"));
+        WebElement Lists = _driver.findElement(By.id("toggle-inputs-lists-"));
         Lists.click();
         //create prospect button
-        WebElement createprospect = driver.findElement(By.cssSelector("input[type = 'submit']"));
+        WebElement createprospect = _driver.findElement(By.cssSelector("input[type = 'submit']"));
         createprospect.click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        _driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //send email in text format////////////////////////////////////////////
-        driver.findElement(By.linkText("Prospects")).click();
-        driver.findElement(By.linkText("Emails")).click();
+        _driver.findElement(By.linkText("Prospects")).click();
+        _driver.findElement(By.linkText("Emails")).click();
 
-        driver.findElement(By.id("none_link_create")).click();
+        _driver.findElement(By.id("none_link_create")).click();
         //email name
-        WebElement emailName = driver.findElement(By.id("name"));
+        WebElement emailName = _driver.findElement(By.id("name"));
         emailName.sendKeys(str);
         //compaign
-        WebElement emailcompain = driver.findElement(By.id("campaign_id"));
+        WebElement emailcompain = _driver.findElement(By.id("campaign_id"));
         emailcompain.click();
         emailcompain.sendKeys("Adil Yellow Jackets");
         //subject
-        WebElement subject = driver.findElement(By.id("subject"));
+        WebElement subject = _driver.findElement(By.id("subject"));
         subject.sendKeys(str);
 
         //format type
-        WebElement format = driver.findElement(By.id("message_format"));
-        //format.click();
-        //format.click();
+        WebElement formatdropdown = _driver.findElement(By.id("message_format"));
+        formatdropdown.isSelected();
 
 
         //sendemail;
         //driver.findElement(By.xpath("//input[@type='button'][onclick='ckeditorSave'][@name='commit']")).click();
 
         //logout
-        WebElement logout = driver.findElement(By.linkText("pardot.applicant@pardot.com"));
+        WebElement logout = _driver.findElement(By.linkText("pardot.applicant@pardot.com"));
         logout.click();
-        driver.findElement(By.linkText("Sign Out")).click();
+        _driver.findElement(By.linkText("Sign Out")).click();
 
-        driver.close();
-        driver.quit();
+
 
         return str;
 
     }
-
 
     public static String GenerateNames() {
 
@@ -170,6 +169,13 @@ public class MyTest {
 
         result += Integer.toString(rnd.nextInt(99));
         return result;
+
+    }
+
+    public static void closeFirefoxBrowser(){
+
+        _driver.close();
+        _driver.quit();
 
     }
 
